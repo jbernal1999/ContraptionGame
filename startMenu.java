aa026aad00ba028941c9ddf6b97880a92d89c0ab
jbernal1999 <64111186+jbernal1999@users.noreply.github.com>
2022-09-10 23:14:44 -0700

    Update startMenu.java

--- startMenu.java
+++ startMenu.java
@@ -5,22 +5,37 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Orientation; 
 import javafx.geometry.Pos; 
 import javafx.scene.Scene; 
-import javafx.scene.control.*; 
+import javafx.scene.control.*;
+import javafx.scene.layout.*; 
 import javafx.scene.text.TextAlignment;
-import javafx.scene.layout.TilePane; 
 import javafx.stage.Stage;
 import java.awt.Component;
 import javafx.geometry.*;
+import javafx.scene.text.*;
 
 public class startMenu extends Application
 {
-   //set here so pane doesn't change?
+    public static void main(String[] args)
+   {
+      launch(args);
+   }
+   
+   //set here so pane doesn't change
    int paneWidth = 650;
    int paneHeight = 650;
+   
+   //change stage name so there's no confusion
+   Stage mainWindow;
+   //create additional scene for level
+   Scene level1, level2, level3, level4;
+   
    public void start(Stage stage)
    {
+      //change stage name
+      mainWindow = stage; 
+       
       //create application window(pane)
-      TilePane window = new TilePane(Orientation.VERTICAL);
+      VBox window = new VBox(10);
       //set size of window
       window.setPrefSize(paneWidth, paneHeight);
       //set color
@@ -28,17 +43,17 @@ public class startMenu extends Application
       
       
       //padding insets to center
-      //insets = (top, right, bottom, left)
-      window.setPadding(new Insets(30, 200, 20, 200));
-      //set vgap = pace between rows
-      window.setVgap(10);
-      
+      //insets = (top, left, bottom, right)
+      window.setPadding(new Insets(30, 250, 30, 250));
       
       //add title - create label
-      Label title = new Label("GAME TITLE FOR SOMEONE TO COME UP WITH");
+      Label title = new Label("STUDENT DROPOUT:)");
       //add title to window
       window.getChildren().addAll(title);
       title.setTextAlignment(TextAlignment.CENTER);
+      title.setPadding(new Insets(20,0,10,0));
+      //set title font
+      title.setFont(Font.font("Impact", FontWeight.BOLD, FontPosture.REGULAR, 12));
       
       //create buttons
       String startString = "START BUTTON";
@@ -48,32 +63,31 @@ public class startMenu extends Application
       
       //set size of buttons
       startButton.setMinWidth(125);
-      startButton.setMinHeight(75);
-      
+      startButton.setMinHeight(50);
       loadButton.setMinWidth(125);
-      loadButton.setMinHeight(75);
+      loadButton.setMinHeight(50);
       
       startButton.setStyle("-fx-text-fill: black");
       loadButton.setStyle("-fx-text-fill: black");
       
-      startButton.setTextAlignment(TextAlignment.CENTER);
-      loadButton.setTextAlignment(TextAlignment.CENTER);
+      //startButton.setTextAlignment(TextAlignment.CENTER);
+      //loadButton.setTextAlignment(TextAlignment.CENTER);
+      startButton.setAlignment(Pos.CENTER);
+      loadButton.setAlignment(Pos.CENTER);
       window.getChildren().add(startButton);
       window.getChildren().add(loadButton);
       
-      window.setHgap(10);
+      //window.setHgap(10);
       
-      Scene scene = new Scene(window, paneWidth, paneHeight);
+      Scene startPage = new Scene(window, paneWidth, paneHeight);
+      
+      //set action on start button
+      startButton.setOnAction(e -> mainWindow.setScene(level1));
       
       //show everything
-      stage.setScene(scene);
-      stage.setTitle("START MENU");
-      stage.show();
+      mainWindow.setScene(startPage);
+      mainWindow.setTitle("START MENU");
+      mainWindow.show();
       
    }
-   public static void main(String[] args)
-   {
-      launch(args);
-   }
-
 }
